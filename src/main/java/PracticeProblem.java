2d3255e92eeb3ce9091272d39f4c8e175866238f
justin-chu-ics4u-03-25 <justin.chu4@student.tdsb.on.ca>
2025-02-27 05:58:20 +0000

    My answers

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,5 +4,42 @@ public class PracticeProblem {
 
 	}
 
+	public static int[] createIntArray() {
+		int[] array = new int[5];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = i + 1;
+		}
+		return array;
+	}
+
+	public static String[] createArray(String string1, String string2, String string3, String string4) {
+		String[] array = {string1, string2, string3, string4};
+		return array;
+	}
+
+	public static int findValue(int value, int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == value) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static int findThirdValue(String value, String[] array) {
+		int count = 0;
+
+		for (int i = 0; i < array.length; i++) {
+			if (array[i].equals(value)) {
+				count++;
+
+				if (count == 3) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+
 
 }
